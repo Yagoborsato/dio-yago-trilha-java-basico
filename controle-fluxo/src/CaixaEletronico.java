6ff4bd49ce353a465b89f82712e7bc94a608f90e
Borsato <yagoborsato@gmail.com>
2024-06-03 11:00:15 -0300

    adicionando estruturas de repetição

--- controle-fluxo/src/CaixaEletronico.java
+++ controle-fluxo/src/CaixaEletronico.java
@@ -1,12 +1,15 @@
 public class CaixaEletronico {
     public static void main(String[] args) {
         double saldo = 25.0;
-        double valorSolicitado = 26.0;
+        double valorSolicitado = 22.0;
 
-        if(valorSolicitado < saldo) 
+        if(valorSolicitado < saldo){
+
             saldo = saldo - valorSolicitado;
+            System.out.println("novo saldo: " + saldo);
 
-            System.out.println(saldo);
+        }else
+        System.out.println("saldo insuficiente");
         
     }
 }
